9b58d612f0200e546f1c6745c150351a63434589
Andrew <andrewbulatao77@gmail.com>
2026-09-04 18:32:19 -0400

    Fixed bug with shooting star placement randomizer.

--- client/build/js/shootingStar.js
+++ client/build/js/shootingStar.js
@@ -8,15 +8,28 @@ const starColors = [
   "#c8f7ff"
 ];
 
-shootingStars.forEach(star => {
+function randomizeStar(star) {
   const randomY = Math.random() * 75 + 5;
   const randomColor = starColors[Math.floor(Math.random() * starColors.length)];
-  const randomDelay = Math.random() * 5;
   const randomDuration = Math.random() * 2 + 4;
+  const randomDelay = Math.random() * 5;
 
+  star.style.animation = "none";
+  star.style.opacity = "0";
   star.style.left = "-100px";
   star.style.top = `${randomY}%`;
   star.style.background = `linear-gradient(90deg, ${randomColor}, transparent)`;
-  star.style.animationDelay = `${randomDelay}s`;
-  star.style.animationDuration = `${randomDuration}s`;
+
+  setTimeout(() => {
+    star.style.animation = `shoot ${randomDuration}s ease-in`;
+    star.style.opacity = "1";
+  }, randomDelay * 1000);
+}
+
+shootingStars.forEach(star => {
+  star.addEventListener("animationend", () => {
+    randomizeStar(star);
+  });
+
+  randomizeStar(star);
 });
