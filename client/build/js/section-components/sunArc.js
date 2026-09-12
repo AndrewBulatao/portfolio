c08e17a859738faf6bd6d93115ec0ed001a17b3e
Andrew <andrewbulatao77@gmail.com>
2026-09-12 11:31:32 -0400

    Added sun shimmer on water.

--- client/build/js/section-components/sunArc.js
+++ client/build/js/section-components/sunArc.js
@@ -1,4 +1,6 @@
 const sun = document.querySelector(".sun");
+const sunReflection = document.querySelector(".sun-reflection");
+
 const screenWidth = window.innerWidth;
 const screenHeight = window.innerHeight;
 const startX = -60;
@@ -10,23 +12,35 @@ const endY = screenHeight * 0.6;
 const animationDuration = 75000;
 let startTime = null;
 
-
 function animateSun(currentTime) {
   if (!startTime) {
     startTime = currentTime;
   }
+  // Find how much time has passed
   const elapsedTime = currentTime - startTime;
   let progress = elapsedTime / animationDuration;
   if (progress > 1) {
     progress = 1;
   }
+
+  // Calculate
+  
+  //horizontal position
   const x = startX + (endX - startX) * progress;
+  // vertical position to create an arc
   const y = startY + (peakY - startY) * (4 * progress * (1 - progress));
+  
+  // Move the sun across the screen
   sun.style.left = `${x}px`;
   sun.style.top = `${y}px`;
+  // Move the reflection underneath the sun
+  sunReflection.style.left = `${x}px`;
+  // Restart the animation when the sun reaches the end
   if (progress >= 1) {
     startTime = currentTime;
   }
+  
+  // Continue the animation
   requestAnimationFrame(animateSun);
 }
 requestAnimationFrame(animateSun);
