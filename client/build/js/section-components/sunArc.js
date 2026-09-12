e6bed82d651271953a287bf9cf7c06e2054e7dec
Andrew <andrewbulatao77@gmail.com>
2026-09-12 11:11:01 -0400

    Fixed sun arc bug of start and end position.

--- client/build/js/section-components/sunArc.js
+++ client/build/js/section-components/sunArc.js
@@ -1,11 +1,11 @@
 const sun = document.querySelector(".sun");
 const screenWidth = window.innerWidth;
 const screenHeight = window.innerHeight;
-const startX = 0;
+const startX = -60;
 const startY = screenHeight * 0.6;
 const peakX = screenWidth / 2;
 const peakY = screenHeight * 0.05;
-const endX = screenWidth;
+const endX = screenWidth + 60;
 const endY = screenHeight * 0.6;
 const animationDuration = 15000;
 let startTime = null;
@@ -24,8 +24,9 @@ function animateSun(currentTime) {
   const y = startY + (peakY - startY) * (4 * progress * (1 - progress));
   sun.style.left = `${x}px`;
   sun.style.top = `${y}px`;
-  if (progress < 1) {
-    requestAnimationFrame(animateSun);
+  if (progress >= 1) {
+    startTime = currentTime;
   }
+  requestAnimationFrame(animateSun);
 }
 requestAnimationFrame(animateSun);
