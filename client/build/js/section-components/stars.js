364a799ea6784378043eb6c3669e623c828a83bd
Andrew <andrewbulatao77@gmail.com>
2026-09-15 15:43:42 -0400

    Redesigning about me -> projects.

--- client/build/js/section-components/stars.js
+++ client/build/js/section-components/stars.js
@@ -1,18 +1,20 @@
-const stars = document.querySelector(".stars");
+const starsContainers = document.querySelectorAll(".stars");
 
-for (let i = 0; i < 50; i++) {
-  const star = document.createElement("div");
-  star.classList.add("star");
+starsContainers.forEach(stars => {
+  for (let i = 0; i < 50; i++) {
+    const star = document.createElement("div");
+    star.classList.add("star");
 
-  const size = Math.random() * 3 + 1;
-  const x = Math.random() * 100;
-  const y = Math.random() * 100;
+    const size = Math.random() * 3 + 1;
+    const x = Math.random() * 100;
+    const y = Math.random() * 100;
 
-  star.style.width = `${size}px`;
-  star.style.height = `${size}px`;
-  star.style.left = `${x}%`;
-  star.style.top = `${y}%`;
-  star.style.animationDelay = `${Math.random() * 3}s`;
-
-  stars.appendChild(star);
-}
+    star.style.width = `${size}px`;
+    star.style.height = `${size}px`;
+    star.style.left = `${x}%`;
+    star.style.top = `${y}%`;
+    
+    star.style.animationDelay = `${Math.random() * 3}s`;
+    stars.appendChild(star);
+  }
+});
